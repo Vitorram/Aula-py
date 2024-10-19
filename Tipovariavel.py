2152ebd6027ea74d12396f81112064aeeed094e8
Vitorram <vitorramosmeneses11@gmail>
2024-10-19 16:28:28 -0300

    hello word

---
+++ Tipovariavel.py
@@ -0,0 +1,8 @@
+codigo = 10
+salario = 1500.00
+nome ='jose'
+situação = True 
+tipo = type (salario)
+print (salario)
+print (tipo)
+print("codigo: "+ str (codigo) +"\nnome: ",nome, "\nsalario atual: ",salario)
